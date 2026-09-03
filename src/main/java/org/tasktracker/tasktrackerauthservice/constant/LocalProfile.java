685de28cf84e8cb7513c48ab5eebce72352a8b92
mthbttrfl <mthbttrfl@google.com>
2026-09-03 16:23:14 +0300

    Basic Application Profiles

---
+++ src/main/java/org/tasktracker/tasktrackerauthservice/constant/LocalProfile.java
@@ -0,0 +1,10 @@
+package org.tasktracker.tasktrackerauthservice.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class LocalProfile {
+
+    public static final String DEV = "dev";
+    public static final String PROD = "prod";
+}
