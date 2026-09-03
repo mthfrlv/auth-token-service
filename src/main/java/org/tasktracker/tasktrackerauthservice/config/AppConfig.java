f654f7d38960a6e86d27a450dd66d9ae62e4d593
mthbttrfl <mthbttrfl@google.com>
2026-09-03 16:23:51 +0300

    Enabling property scanning

---
+++ src/main/java/org/tasktracker/tasktrackerauthservice/config/AppConfig.java
@@ -0,0 +1,9 @@
+package org.tasktracker.tasktrackerauthservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan
+public class AppConfig {
+}
